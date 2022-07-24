dafc9533af62acc4a9179f4b1862ad2bfa0ffd39
Garry <tahinjanaharygarry@gmail.com>
2022-07-24 14:38:35 +0300

    create maximisation algorithm, done

--- algo.py
+++ algo.py
@@ -3,7 +3,8 @@ import numpy as np
 
 class Demoucron:
     def __init__(self, matrice, choix: str):
-        self._matrice = matrice
+        self._origin = np.copy(matrice)
+        self._matrice: np.ndarray = matrice
         self.comparer_elem = Demoucron.notNan if choix == 'minimiser' else Demoucron.greater
         self.comparer_vecteur = min if choix == 'minimiser' else max
         self.choix = choix
@@ -61,22 +62,21 @@ class Demoucron:
 
     @property
     def maximiser(self):
-        ligne = 0
-        chemin = [ligne+1]
-        while ligne < self.sommets - 1:
-            i = 0
-            min_value = self._matrice[ligne, i]
-            for item in self._matrice[ligne]:
-                if min_value > 0 and item > 0:
-                    if item < min_value:
-                        min_value = item
-                        ligne = i
-                elif min_value == 0 and item > 0:
-                    min_value = item
-                    ligne = i
-                i += 1
-            chemin.append(ligne+1)
-        return chemin
+        column = self.sommets-1
+        res = []
+        while column > 0:
+            marquages = np.amax(self._matrice, axis=0)
+            max_val = marquages[column]
+            distances = self._origin[:, column]
+            indices = [i for i, v in enumerate(distances) if v > 0]
+            for i in indices:
+                if distances[i] + marquages[i] == max_val:
+                    res.append(column)
+                    column = i
+                    break
+        res.append(0)
+        res.reverse()
+        print(res)
 
     def find_path(self) -> list[int]:
         return getattr(self, self.choix)
@@ -95,15 +95,31 @@ class Demoucron:
 
 
 a = np.array([
-    [None, 3, 8, 6, None, None],
-    [None, None, None, 2, 6, None],
-    [None, None, None, None, 1, None],
-    [None, None, 2, None, None, 7],
-    [None, None, None, None, None, 2],
-    [None, None, None, None, None, None]
+    [0, 3, 0, 5, 0, 0, 0],
+    [0, 0, 4, 2, 6, 0, 0],
+    [0, 0, 0, 0, 4, 0, 5],
+    [0, 0, 3, 0, 0, 7, 0],
+    [0, 0, 0, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 0, 0, 0],
 ], dtype=np.float64)
 
+m = np.array([[  0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 15, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0],
+    [0, 0, 8, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 8, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 12, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ], dtype=np.float64)
 
-d = Demoucron(a, 'minimiser')
-print(d._matrice)
-print(d.find_path())
+d = Demoucron(m, 'maximiser')
+d.find_path()
